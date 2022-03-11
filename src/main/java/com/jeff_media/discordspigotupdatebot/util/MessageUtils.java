6d744c57a626a6636067c3bccfc74f626ff31da7
mfnalex <1122571+mfnalex@users.noreply.github.com>
2022-03-11 17:43:19 +0100

    fixed exception when not using thumbnails in plugins.yml

--- src/main/java/com/jeff_media/discordspigotupdatebot/util/MessageUtils.java
+++ src/main/java/com/jeff_media/discordspigotupdatebot/util/MessageUtils.java
@@ -8,16 +8,13 @@ public enum MessageUtils {
 
     public static String applyPlaceholders(final Plugin plugin, String text) {
         if(text == null) return null;
-        text = text.replace("%name%",plugin.name())
+        return text.replace("%name%",plugin.name())
                 .replace("%version%",plugin.version())
                 .replace("%spigot_link%", plugin.getSpigotLink())
                 .replace("%changelog_link%", plugin.getUpdateLink())
                 .replace("%download_link%", plugin.getDownloadLink())
-                .replace("%date%", getTime(plugin.timestamp()));
-        if(plugin.thumbnail() != null) {
-            text = text.replace("%thumbnail%", plugin.thumbnail());
-        }
-        return text;
+                .replace("%date%", getTime(plugin.timestamp()))
+                .replace("%thumbnail%", plugin.thumbnail() == null ? "" : plugin.thumbnail());
     }
 
 
